5de15f7773122fc3d91d1b88c945616ddea705cf
Ballred95 <blake.allred95@icloud.com>
2020-06-17 23:57:19 -0600

    Added javascript variable style into render function and displayed.

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,8 +2,16 @@ import React, {Component} from 'react'
 
 export default class Card extends Component {
     render(){
+        const buttonStyle = {
+            backgroundColor: 'yellow',
+            padding: '10px',
+            borderRadius: '15%',
+            border: 'none'
+        }
         return(
-            <div></div>
+            <div>
+                <button style={buttonStyle}>Night and Day</button>
+            </div>
         )
     }
 }
--- src/components/app.js
+++ src/components/app.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
+import Card from './Card'
 
 export default class App extends Component {
   render() {
     return (
       <div className='app'>
-        <h1>DevCamp React Starter</h1>
-        <h2>React Redux Router</h2>
+        <h1>NIGHT</h1>
+        <Card />
       </div>
     );
   }
