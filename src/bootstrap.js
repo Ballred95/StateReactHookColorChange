2c9a04dca7219c44a5f63825a2a12d399e8c9335
Ballred95 <blake.allred95@icloud.com>
2020-06-20 20:34:51 -0600

    reconfiguring project for react and scss styles

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -5,7 +5,7 @@ import { createStore, applyMiddleware } from "redux";
 import { BrowserRouter } from "react-router-dom";
 import App from "./components/app";
 import reducers from "./reducers";
-import Card from '../src/components/Card'
+
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 
